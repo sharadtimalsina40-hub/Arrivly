5fee59d9f41a7263038c9592a608c9f9a1ae7be5
“Sharad <sharadtimalsina40@gmail.com>
2026-05-27 21:23:21 +1000

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://arrivly.com.au',
+  integrations: [sitemap()],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
